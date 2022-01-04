74504cae7d7f76257b5cbe9e45ce8246e205ce00
This2sho <zx00018@naver.com>
2022-01-05 00:53:11 +0900

    Test: 기본 키 매핑 테스트
    IDENTITY, SEQUENCE 매핑 확인 및 테스트

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -17,42 +17,14 @@ public class JpaMain {
         tx.begin();
 
         try{
-//            Member findMember = em.find(Member.class, 1L);
-//            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-//                    .setFirstResult(5)
-//                    .setMaxResults(8)
-//                    .getResultList();
-//
-//            for (Member member : result) {
-//                System.out.println("member.name = " + member.getName());
-//            }
 
-//            // 비영속
-//            Member member = new Member();
-//            member.setId(101L);
-//            member.setName("HelloJPA");
-//
-//            // 영속
-//            System.out.println("=== BEFORE ===");
-//            em.persist(member);
-//            System.out.println("=== AFTER ===");
+            Member member = new Member();
+            member.setUsername("C");
 
-            // 영속
-//            Member findMember1 = em.find(Member.class, 101L);
-//            Member findMember2 = em.find(Member.class, 101L); // 1차 캐시에 올려져있어서 쿼리문 안날림.
-//
-//            System.out.println("result = " + (findMember1 == findMember2));
-
-//            Member member1 = new Member(150L, "A");
-//            Member member2 = new Member(160L, "B");
-//
-//            em.persist(member1);
-//            em.persist(member2);
-//
-//            System.out.println("=====================");
-
-            Member member = em.find(Member.class, 150L);
-            member.setName("ZZZZZ");
+            System.out.println("===========");
+            em.persist(member);
+            System.out.println("member.getId() = " + member.getId());
+            System.out.println("===========");
 
             tx.commit();
         } catch (Exception e){
